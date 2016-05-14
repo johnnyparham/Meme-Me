29928fba610e2fc6b34d59889503418ecca3e008
johnnyparham <johnny.parham@gmail.com>
2016-05-14 14:28:03 -0400

    Setup ViewController Outlets and Actions

--- MemeMe version 1.0/ViewController.swift
+++ MemeMe version 1.0/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    @IBOutlet weak var topToolBar: UIToolbar!
+    @IBOutlet weak var bottomToolBar: UIToolbar!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: -
+    //MARK: Action functions
+    
+    
+    // open the image picker controller from the library
+    @IBAction func pickImageFromLibrary(sender: UIBarButtonItem) {
+        
+    }
+    
+    // open the image picker controller from the camera
+    @IBAction func pickImageFromCamera(sender: UIBarButtonItem) {
+        
+    }
+    
+    // share the created meme image
+    @IBAction func shareMemeImage(sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    // reset UI
+    @IBAction func resetUI(sender: UIBarButtonItem) {
+        
+    }
 
 }
 
